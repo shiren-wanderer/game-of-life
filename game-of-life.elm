bc825916d50a29189836a3c4ddab001d54ac5d8b
Gom Dev <bajstemp@gmail.com>
2016-10-17 00:16:24 -0700

    Adding Range input to adjust speed in real time

--- game-of-life.elm
+++ game-of-life.elm
@@ -1,24 +1,27 @@
 module Main exposing (..)
 
-import Html exposing (div, span, text, button)
-import Html.Attributes exposing (style)
+import Html exposing (div, span, text, button, input)
+import Html.Attributes as Attr exposing (style, type', min, max, value)
 import Html.App exposing (program)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Matrix exposing (..)
 import Matrix.Random exposing (..)
 import Time exposing (Time, millisecond)
 import Random exposing (..)
+import String
 
 
 type alias Model =
     { size : Int
     , field : Matrix Bool
     , running : Bool
+    , speed : Float
     }
 
 
 type Msg
     = Update Time
+    | UpdateSpeed String
     | Next
     | OnOff
     | Reset
@@ -28,16 +31,26 @@ type Msg
 main : Program Never
 main =
     program
-        { init = init 50
+        { init = init initSize initSpeed
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
 
 
-init : Int -> ( Model, Cmd Msg )
-init size =
-    Model 0 (Matrix.matrix size size (\loc -> False)) False ! [ Random.generate (NewField size) (randomField size) ]
+init : Int -> Float -> ( Model, Cmd Msg )
+init size speed =
+    Model 0 (Matrix.matrix size size (\loc -> False)) False speed ! [ Random.generate (NewField size) (randomField size) ]
+
+
+initSpeed : Float
+initSpeed =
+    500
+
+
+initSize : Int
+initSize =
+    50
 
 
 randomField : Int -> Generator (Matrix Bool)
@@ -54,14 +67,26 @@ update msg model =
         Update x ->
             { model | field = nextGenerationMatrix model } ! []
 
+        UpdateSpeed newSpeedStr ->
+            let
+                intConvResult =
+                    String.toFloat newSpeedStr
+            in
+                case intConvResult of
+                    Result.Ok newSpeed ->
+                        { model | speed = newSpeed } ! []
+
+                    Result.Err err ->
+                        { model | speed = initSpeed } ! []
+
         NewField size field ->
-            Model size field False ! []
+            Model size field False model.speed ! []
 
         OnOff ->
             { model | running = not model.running } ! []
 
         Reset ->
-            init model.size
+            init model.size model.speed
 
 
 nextGenerationMatrix : Model -> Matrix Bool
@@ -121,7 +146,7 @@ surroundLocations ( x, y ) =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     if model.running then
-        Time.every (10 * millisecond) Update
+        Time.every (model.speed * millisecond) Update
     else
         Sub.none
 
@@ -131,18 +156,29 @@ view model =
     div []
         [ div []
             [ button [ style buttonStyle, onClick Next ] [ text "Next" ]
-            , button [ style buttonStyle, onClick OnOff ]
-                [ text
-                    (if model.running then
-                        "Stop"
-                     else
-                        "Run"
-                    )
-                ]
+            , button [ style buttonStyle, onClick OnOff ] [ text <| onOffButtonText model.running ]
             , button [ style buttonStyle, onClick Reset ] [ text "Reset" ]
+            , input
+                [ type' "range"
+                , Attr.min "100"
+                , Attr.max "1000"
+                , Attr.step "100"
+                , value <| toString model.speed
+                , onInput UpdateSpeed
+                ]
+                []
+            , span [] [ text <| (toString <| model.speed / 1000) ++ "sec" ]
             ]
         , div [] <| List.map printRow <| Matrix.toList model.field
         ]
+
+
+onOffButtonText : Bool -> String
+onOffButtonText running =
+    if running then
+        "Stop"
+    else
+        "Run"
 
 
 printRow : List Bool -> Html.Html Msg
